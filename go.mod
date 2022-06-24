be5471738ce12bef5cf5563dec53b943282f8eb7
themitchnz <jamesmitch@gmail.com>
2022-06-24 21:47:48 +1200

    added raylib

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/themitchnz/psycho-sms
 
 go 1.18
+
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20220601234331-25dea46fc2d9 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
 func main() {
-	println("Hello, world!")
+	rl.InitWindow(256, 224, "Psycho SMS")
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+
+		rl.DrawText("Wow!, Nothing works yet!", 20, 20, 15, rl.LightGray)
+
+		rl.EndDrawing()
+	}
+
+	rl.CloseWindow()
 }
